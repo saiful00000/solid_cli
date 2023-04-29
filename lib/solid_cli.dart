d512de9422adb8e7277498d8ca5752c76767ff4a
saiful00000 <saiful161003@gmail.com>
2023-04-29 18:23:32 +0600

    Initial commit

---
+++ lib/solid_cli.dart
@@ -0,0 +1,10 @@
+/// solid_cli, The ultimate cli for developing flutter apps using SOLID principles.
+///
+/// ```sh
+/// # activate solid_cli
+/// dart pub global activate solid_cli
+///
+/// # see usage
+/// solid --help
+/// ```
+library solid_cli;
